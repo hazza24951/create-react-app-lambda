99ebf48414ea6196e0d4b9534b0514cc884dcd92
hazza24951 <101466844+hazza24951@users.noreply.github.com>
2022-10-11 12:16:23 +0100

    Add files via upload

---
+++ src/components/sections/About.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import Carousel from '../Carousel'
+import Button from '../Button'
+import {dark} from '../../styles/Themes';
+
+const Section = styled.section`
+min-height: 100vh;
+width: 100%;
+background-color: ${props => props.theme.text};
+display: flex;
+justify-content: center;
+align-items: center;
+position: relative;
+`
+const Container = styled.div`
+width: 75%;
+margin: 0 auto;
+/* background-color: lightblue */
+
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const Box = styled.div`
+width: 50%;
+height: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+margin: 0 auto;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontxl};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontlg};
+
+}
+`
+const SubText = styled.p`
+  font-size: ${(props) => props.theme.fontlg};
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+margin: 1rem auto;
+font-weight:400;
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+  font-size: ${(props) => props.theme.fontmd};
+
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontmd};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+
+`
+const SubTextLight = styled.p`
+  font-size: ${(props) => props.theme.fontmd};
+  color: ${(props) => `rgba(${props.theme.bodyRgba},0.6)`};
+  align-self: flex-start;
+  width: 80%;
+margin: 1rem auto;
+font-weight:400;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align:center;
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+@media (max-width: 40em){
+  font-size: ${(props) => props.theme.fontsm};
+
+}
+@media (max-width: 30em){
+  font-size: ${(props) => props.theme.fontxs};
+
+}
+
+`
+const ButtonContainer = styled.div`
+ width: 80%;
+ margin: 1rem auto;
+ display: flex;
+  align-self: flex-start;
+
+  @media (max-width: 64em){
+width: 100%;
+
+a{
+  margin: 0 auto;
+}
+}
+
+`
+
+
+const About = () => {
+    return(
+        <Section id="about">
+            <Box> <Carousel /> </Box>
+            <Box>
+              <Title>
+                Welcome to Wild Animals Club🐯🙈.
+                </Title>  
+                <SubText>
+                    The Wild Animals Club is a private collection of  NFTs-unique digitcal collectibles. The Wild Animals are stored on the Solana blockchain.
+                </SubText>
+                <SubTextLight>
+                    With more than 3000+ hand drawn Animals, each NFT is unique and comes with a membership to an exlusive DAO group.
+                </SubTextLight>
+                <ButtonContainer>
+        <ThemeProvider theme={dark}>
+        <Button text="JOIN OUR PROJECT" link="https://discord.gg/KmEQj3Mzaj" />
+        </ThemeProvider>
+        </ButtonContainer>
+            </Box>
+        </Section>
+    )
+}
+
+export default About
